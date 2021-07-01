b1c3a7486d342805ee424b88347940ae8aa6d89b
Luiz Marcel <lmgaa@discente.ifpe.edu.br>
2021-07-01 17:05:13 -0300

    Parâmetros da classe exercício

--- src/main/java/model/Exercicio.java
+++ src/main/java/model/Exercicio.java
@@ -1,5 +1,29 @@
 package model;
 
 public class Exercicio {
+	
+	private String nome;
+	private int repeticoes;
+	private int series;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getRepeticoes() {
+		return repeticoes;
+	}
+	public void setRepeticoes(int repeticoes) {
+		this.repeticoes = repeticoes;
+	}
+	public int getSeries() {
+		return series;
+	}
+	public void setSeries(int series) {
+		this.series = series;
+	}
+	
 
 }
